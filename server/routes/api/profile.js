e2bd4367dd6d87e3a5a9c75513c7ba2b7bd2cb1e
Your Name <akhouri.a@northeastern.edu>
2024-12-02 17:46:40 -0500

    Add experience route and delete experience route added.

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -146,4 +146,78 @@ router.delete('/', auth, async (req, res) => {
       res.status(500).send('Server Error');
     }
   });
+
+// @route    PUT api/profile/experience
+// @desc     Add profile experience
+// @access   Private
+router.put(
+    '/experience',
+    auth,
+    [
+      check('title', 'Title is required').notEmpty(),
+      check('company', 'Company is required').notEmpty(),
+      check('from', 'From date is required and needs to be from the past')
+        .notEmpty()
+        .custom((value, { req }) => {
+          const fromDate = new Date(value);
+          const toDate = req.body.to ? new Date(req.body.to) : null;
+  
+          // Ensure 'from' is in the past
+          if (fromDate >= new Date()) {
+            throw new Error('From date must be in the past');
+          }
+  
+          // Ensure 'from' is before 'to' if 'to' is provided
+          if (toDate && fromDate >= toDate) {
+            throw new Error("'From' date must be before 'To' date");
+          }
+  
+          return true;
+        })
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+      try {
+        const profile = await Profile.findOne({ user: req.user.id });
+  
+        // Add the new experience to the beginning of the experience array
+        profile.experience.unshift(req.body);
+  
+        // Save the updated profile
+        await profile.save();
+  
+        // Respond with the updated profile
+        res.json(profile);
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+      }
+    }
+  );
+  
+
+// @route    DELETE api/profile/experience/:exp_id
+// @desc     Delete experience from profile
+// @access   Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+    try {
+      const foundProfile = await Profile.findOne({ user: req.user.id });
+  
+      foundProfile.experience = foundProfile.experience.filter(
+        (exp) => exp._id.toString() !== req.params.exp_id
+      );
+  
+      await foundProfile.save();
+      return res.status(200).json(foundProfile);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ msg: 'Server error' });
+    }
+  });
+  
 module.exports = router;
