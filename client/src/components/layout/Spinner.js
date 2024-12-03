60eebf5b66b1cdadab317c3f4c2ebfa1e98f5c9c
kshitijkumar-northestern <kumar.ks@northeastern.edu>
2024-12-03 11:17:18 -0500

    added_layout_components

---
+++ client/src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Spinner.css";
+
+const Spinner = () => (
+  <div className="spinner-overlay">
+    <div className="spinner">
+      <div className="spinner-circle"></div>
+    </div>
+  </div>
+);
+
+export default Spinner;
